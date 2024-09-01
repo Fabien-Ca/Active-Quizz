3f7286f0429e84d249eaa6279b707c65c6744a41
Fabien-Ca <84989360+Fabien-Ca@users.noreply.github.com>
2024-09-01 09:49:06 +0200

    Add files via upload

---
+++ lang/fr/activequiz.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   mod_activequiz
+ * @author    John Hoopes <moodle@madisoncreativeweb.com>
+ * @author    Davo Smith
+ * @copyright 2014 University of Wisconsin - Madison
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/****************** modif PRT 2406 ******************/
+$string['show_stats'] = 'Afficher la répartition des réponses';
+$string['hide_stats'] = 'Cacher la répartition des réponses';
+
+$string['feedbackintro'] = 'Merci d’attendre que l’enseignant démarre la question suivante.';
+$string['save'] = 'Valider';
+
+/**************** Fin modif PRT 2406 ****************/
